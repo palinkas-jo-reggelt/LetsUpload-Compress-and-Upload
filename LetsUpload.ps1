cfd5cf16b744a5b0df49455a87abc93ff24ad50e
Ad Min <admin@123.dynu.com>
2020-10-30 18:10:50 -0400

    housekeeping

--- LetsUpload.ps1
+++ LetsUpload.ps1
@@ -71,11 +71,18 @@ Function EmailResults {
 	}
 }
 
-Function ElapsedTime ($TimeSpan) {
+Function ElapsedTime ($EndTime) {
+	$TimeSpan = New-Timespan $EndTime
 	If (([int]($TimeSpan).Hours) -eq 0) {$Hours = ""} ElseIf (([int]($TimeSpan).Hours) -eq 1) {$Hours = "1 hour "} Else {$Hours = "$([int]($TimeSpan).Hours) hours "}
 	If (([int]($TimeSpan).Minutes) -eq 0) {$Minutes = ""} ElseIf (([int]($TimeSpan).Minutes) -eq 1) {$Minutes = "1 minute "} Else {$Minutes = "$([int]($TimeSpan).Minutes) minutes "}
 	If (([int]($TimeSpan).Seconds) -eq 1) {$Seconds = "1 second"} Else {$Seconds = "$([int]($TimeSpan).Seconds) seconds"}
-	Return "$Hours$Minutes$Seconds"
+	
+	If (($TimeSpan).TotalSeconds -lt 1) {
+		$Return = "less than 1 second"
+	} Else {
+		$Return = "$Hours$Minutes$Seconds"
+	}
+	Return $Return
 }
 
 <###   BEGIN SCRIPT   ###>
@@ -132,7 +139,7 @@ Catch {
 	EmailResults
 	Exit
 }
-Debug "Archive creation finished in $(ElapsedTime (New-Timespan $StartArchive))"
+Debug "Archive creation finished in $(ElapsedTime $StartArchive)"
 Debug "Wait a few seconds to make sure archive is finished"
 Start-Sleep -Seconds 3
 
@@ -146,6 +153,10 @@ $AuthBody = @{
 }
 Try{
 	$Auth = Invoke-RestMethod -Method GET $URIAuth -Body $AuthBody -ContentType 'application/json; charset=utf-8' 
+	$AccessToken = $Auth.data.access_token
+	$AccountID = $Auth.data.account_id
+	Debug "Access Token : $AccessToken"
+	Debug "Account ID   : $AccountID"
 }
 Catch {
 	Debug "LetsUpload Authentication ERROR : $Error"
@@ -154,10 +165,6 @@ Catch {
 	EmailResults
 	Exit
 }
-$AccessToken = $Auth.data.access_token
-$AccountID = $Auth.data.account_id
-Debug "Access Token : $AccessToken"
-Debug "Account ID   : $AccountID"
 
 <#  Create Folder  #>
 Debug "----------------------------"
@@ -171,32 +178,34 @@ $CFBody = @{
 }
 Try {
 	$CreateFolder = Invoke-RestMethod -Method GET $URICF -Body $CFBody -ContentType 'application/json; charset=utf-8' 
+	$CFResponse = $CreateFolder.response
+	$FolderID = $CreateFolder.data.id
+	$FolderURL = $CreateFolder.data.url_folder
+	Debug "Response   : $CFResponse"
+	Debug "Folder ID  : $FolderID"
+	Debug "Folder URL : $FolderURL"
 }
 Catch {
 	Debug "LetsUpload Folder Creation ERROR : $Error"
-	Email "LetsUpload Folder Creation ERROR : Check Debug Log"
-	Email "LetsUpload Folder Creation ERROR : $Error"
+	Email "[ERROR] LetsUpload Folder Creation : Check Debug Log"
+	Email "[ERROR] LetsUpload Folder Creation : $Error"
 	EmailResults
 	Exit
 }
-$CreateFolder.response
-$FolderID = $CreateFolder.data.id
-$FolderURL = $CreateFolder.data.url_folder
-Debug "Folder ID  : $FolderID"
-Debug "Folder URL : $FolderURL"
 
 <#  Upload Files  #>
 $StartUpload = Get-Date
 Debug "----------------------------"
 Debug "Begin uploading files to LetsUpload"
-$Count = (Get-ChildItem "$BackupLocation\$BackupName").Count
-Debug "There are $Count files to upload"
-$N = 1
+$CountArchVol = (Get-ChildItem "$BackupLocation\$BackupName").Count
+Debug "There are $CountArchVol files to upload"
+$UploadCounter = 1
 
 Get-ChildItem "$BackupLocation\$BackupName" | ForEach {
 
 	$FileName = $_.Name;
 	$FilePath = $_.FullName;
+	$FileSize = $_.Length;
 	
 	$UploadURI = "https://letsupload.io/api/v2/file/upload";
 	Debug "----------------------------"
@@ -207,11 +216,11 @@ Get-ChildItem "$BackupLocation\$BackupName" | ForEach {
 		$FileEnc = [System.Text.Encoding]::GetEncoding('ISO-8859-1').GetString($FileBytes);
 	}
 	Catch {
-			Debug "Error in encoding file $N."
+			Debug "Error in encoding file $UploadCounter."
 			Debug "$Error"
 			Debug " "
 	}
-	Debug "Finished encoding file in $(ElapsedTime (New-TimeSpan $BeginEnc))";
+	Debug "Finished encoding file in $(ElapsedTime $BeginEnc)";
 	$Boundary = [System.Guid]::NewGuid().ToString(); 
 	$LF = "`r`n";
 
@@ -236,10 +245,9 @@ Get-ChildItem "$BackupLocation\$BackupName" | ForEach {
 		"--$Boundary--"
 	) -join $LF
 		
-	Debug "Uploading $FileName - $N of $Count"
+	Debug "Uploading $FileName - $UploadCounter of $CountArchVol"
 	$UploadTries = 1
 	$BeginUpload = Get-Date
-	
 	Do {
 		$Error.Clear()
 		$Upload = $UResponse = $UURL = $USize = $UStatus = $NULL
@@ -248,62 +256,117 @@ Get-ChildItem "$BackupLocation\$BackupName" | ForEach {
 			$UResponse = $Upload.response
 			$UURL = $Upload.data.url
 			$USize = $Upload.data.size
+			$USizeFormatted = "{0,8:N2}" -f (($USize)/1MB)
 			$UStatus = $Upload._status
-			Debug "Upload try $A"
+			$UFileID = $upload.data.file_id
+			If ($USize -ne $FileSize) {Throw "Local and remote filesizes do not match!"}
+			Debug "Upload try $UploadTries"
 			Debug "Response : $UResponse"
+			Debug "File ID  : $UFileID"
 			Debug "URL      : $UURL"
-			Debug "Size     : $USize"
+			Debug "Size     : $USizeFormatted mb"
 			Debug "Status   : $UStatus"
-			Debug "Finished uploading file in $(ElapsedTime (New-TimeSpan $BeginUpload))"
+			Debug "Finished uploading file in $(ElapsedTime $BeginUpload)"
 		} 
 		Catch {
-			Debug "Upload try $A"
-			Debug "[ERROR] : $Error"
+			Debug "Upload try $UploadTries"
+			Debug "[ERROR]  : $Error"
+			If (($USize -gt 0) -and ($UFileID -match '\d+')) {
+				Debug "Deleting file due to size mismatch"
+				$URIDF = "https://letsupload.io/api/v2/file/delete"
+				$DFBody = @{
+					'access_token' = $AccessToken;
+					'account_id' = $AccountID;
+					'file_id' = $UFileID;
+				}
+				Try {
+					$DeleteFile = Invoke-RestMethod -Method GET $URIDF -Body $DFBody -ContentType 'application/json; charset=utf-8' 
+				}
+				Catch {
+					Debug "File delete ERROR : $Error"
+				}
+			}
 		}
-
 		$UploadTries++
 	} Until (($UploadTries -eq ($MaxUploadTries + 1)) -or ($UStatus -match "success"))
 
-
 	If (-not($UStatus -Match "success")) {
-		Debug "Error in uploading file number $N. Check the log for errors."
-		Email "Error in uploading file number $N. Check the log for errors."
+		Debug "Error in uploading file number $UploadCounter. Check the log for errors."
+		Email "[ERROR] in uploading file number $UploadCounter. Check the log for errors."
 		EmailResults
 		Exit
 	}
-
-	$N++
+	$UploadCounter++
 }
-
 Debug "----------------------------"
-Debug "Finished uploading $Count files in $(ElapsedTime (New-TimeSpan $StartUpload))"
+Debug "Finished offsite upload process in $(ElapsedTime $BeginOffsiteUpload)"
+
+<#  Count remote files  #>
+Debug "----------------------------"
+Debug "Counting uploaded files at LetsUpload"
+$URIFL = "https://letsupload.io/api/v2/folder/listing"
+$FLBody = @{
+	'access_token' = $AccessToken;
+	'account_id' = $AccountID;
+	'parent_folder_id' = $FolderID;
+}
+Try {
+	$FolderListing = Invoke-RestMethod -Method GET $URIFL -Body $FLBody -ContentType 'application/json; charset=utf-8' 
+}
+Catch {
+	Debug "LetsUpload Folder Listing ERROR : $Error"
+	Email "[ERROR] LetsUpload Folder Listing : Check Debug Log"
+	Email "[ERROR] LetsUpload Folder Listing : $Error"
+}
+$FolderListingStatus = $FolderListing._status
+$RemoteFileCount = ($FolderListing.data.files.id).Count
+
+<#  Report results  #>
+If ($FolderListingStatus -match "success") {
+	Debug "There are $RemoteFileCount files in the remote folder"
+	If ($RemoteFileCount -eq $CountArchVol) {
+		Debug "----------------------------"
+		Debug "Finished uploading $CountArchVol files in $(ElapsedTime $StartUpload)"
+		Debug "Upload sucessful. $CountArchVol files uploaded to $FolderURL"
+		Email "* Offsite upload of backup archive completed successfully:"
+		Email "* $CountArchVol files uploaded to $FolderURL"
+	} Else {
+		Debug "----------------------------"
+		Debug "Finished uploading in $(ElapsedTime $StartUpload)"
+		Debug "[ERROR] Number of archive files uploaded does not match count in remote folder"
+		Debug "[ERROR] Archive volumes   : $CountArchVol"
+		Debug "[ERROR] Remote file count : $RemoteFileCount"
+		Email "[ERROR] Number of archive files uploaded does not match count in remote folder - see debug log"
+	}
+} Else {
+	Debug "----------------------------"
+	Debug "Error : Unable to obtain file count from remote folder"
+	Email "[ERROR] Unable to obtain uploaded file count from remote folder - see debug log"
+}
 
 <#  Delete old backups  #>
-$FilesToDel = Get-ChildItem -Path $BackupLocation  | Where-Object {$_.LastWriteTime -lt ((Get-Date).AddDays(-$DaysToKeep))}
-$CountDel = $FilesToDel.Count
-If ($CountDel -gt 0) {
-	Debug "Deleting $CountDel items older than $DaysToKeep days"
-	Debug "----------------------------"
-}
-$FilesToDel | ForEach {
-	$FullName = $_.FullName
-	$Name = $_.Name
-	If (Test-Path $_.FullName -PathType Container) {
-		Remove-Item -Force -Recurse -Path $FullName
-		Debug "Deleting folder: $Name"
+If ($DeleteOldBackups) {
+	$FilesToDel = Get-ChildItem -Path $BackupLocation  | Where-Object {$_.LastWriteTime -lt ((Get-Date).AddDays(-$DaysToKeep))}
+	$CountDel = $FilesToDel.Count
+	If ($CountDel -gt 0) {
+		Debug "Deleting $CountDel items older than $DaysToKeep days"
+		Debug "----------------------------"
 	}
-	If (Test-Path $_.FullName -PathType Leaf) {
-		Remove-Item -Force -Path $FullName
-		Debug "Deleting file  : $Name"
+	$FilesToDel | ForEach {
+		$FullName = $_.FullName
+		$Name = $_.Name
+		If (Test-Path $_.FullName -PathType Container) {
+			Remove-Item -Force -Recurse -Path $FullName
+			Debug "Deleting folder: $Name"
+		}
+		If (Test-Path $_.FullName -PathType Leaf) {
+			Remove-Item -Force -Path $FullName
+			Debug "Deleting file  : $Name"
+		}
 	}
 }
 
 <#  Finish up and email results  #>
-Debug "Upload sucessful. $Count files uploaded to $FolderURL"
-Email "Upload sucessful. $Count files uploaded to $FolderURL"
-Email " "
-Debug "Script completed in $(ElapsedTime (New-TimeSpan $StartScript))"
-Email "Script completed in $(ElapsedTime (New-TimeSpan $StartScript))"
 Debug "Sending Email"
 If (($AttachDebugLog) -and (Test-Path $DebugLog) -and (((Get-Item $DebugLog).length/1MB) -gt $MaxAttachmentSize)){
 	Email "Debug log size exceeds maximum attachment size. Please see log file in script folder"
